2b868af8545b03349ccd9979f66f6b0ca18e9f46
burak <burak.colak@appcent.mobi>
2023-12-29 00:45:35 +0300

    🎷 [UPDATE] Cryptokit made it only available for iOS 13 or above.

--- Sources/ACMNetworking/Library/Utils/Crypto/ACMEncryptionUtils.swift
+++ Sources/ACMNetworking/Library/Utils/Crypto/ACMEncryptionUtils.swift
@@ -31,52 +31,76 @@ final class ACMEncryptionUtils {
     }
 
     private func md5(value: String) -> String {
-        Insecure.MD5.hash(data: Data(value.utf8))
-            .map {
-                String(format: encFormat, $0)
-            }
-            .joined()
+        if #available(iOS 13, *) {
+            return Insecure.MD5.hash(data: Data(value.utf8))
+                .map {
+                    String(format: encFormat, $0)
+                }
+                .joined()
+        } else {
+            return ""
+        }
     }
 
     private func base64(value: String) -> String {
-        guard let data = Data(base64Encoded: value) else { return "" }
-        guard let str = String(data: data, encoding: .utf8) else { return "" }
-        return str
+        if #available(iOS 13, *) {
+            guard let data = Data(base64Encoded: value) else { return "" }
+            guard let str = String(data: data, encoding: .utf8) else { return "" }
+            return str
+        } else {
+            return ""
+        }
     }
 
     private func sha1(value: String) -> String {
-        Insecure.SHA1.hash(data: Data(value.utf8))
-            .map {
-                String(format: encFormat, $0)
-            }
-            .joined()
+        if #available(iOS 13, *) {
+            return Insecure.SHA1.hash(data: Data(value.utf8))
+                .map {
+                    String(format: encFormat, $0)
+                }
+                .joined()
+        } else {
+            return ""
+        }
     }
 
     private func sha256(value: String) -> String {
-        guard let data = value.data(using: .utf8) else { return "" }
-        return SHA256.hash(data: data)
-            .map {
-                String(format: encFormat, $0)
-            }
-            .joined()
+        if #available(iOS 13, *) {
+            guard let data = value.data(using: .utf8) else { return "" }
+            return SHA256.hash(data: data)
+                .map {
+                    String(format: encFormat, $0)
+                }
+                .joined()
+        } else {
+            return ""
+        }
     }
 
     private func sha384(value: String) -> String {
-        guard let data = value.data(using: .utf8) else { return "" }
-        return SHA384.hash(data: data)
-            .map {
-                String(format: encFormat, $0)
-            }
-            .joined()
+        if #available(iOS 13, *) {
+            guard let data = value.data(using: .utf8) else { return "" }
+            return SHA384.hash(data: data)
+                .map {
+                    String(format: encFormat, $0)
+                }
+                .joined()
+        } else {
+            return ""
+        }
     }
 
     private func sha512(value: String) -> String {
-        guard let data = value.data(using: .utf8) else { return "" }
-        return SHA512.hash(data: data)
-            .map {
-                String(format: encFormat, $0)
-            }
-            .joined()
+        if #available(iOS 13, *) {
+            guard let data = value.data(using: .utf8) else { return "" }
+            return SHA512.hash(data: data)
+                .map {
+                    String(format: encFormat, $0)
+                }
+                .joined()
+        } else {
+            return ""
+        }
     }
 
     private let encFormat = "%02hhx"
